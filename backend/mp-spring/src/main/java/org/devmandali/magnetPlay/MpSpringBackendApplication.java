038398195d648197597f71d727fffe5f08c5208f
Vedant Barge <bargevedant@gmail.com>
2026-02-15 15:22:35 +0530

    Base(Structure) - Strucutre for project - Go, Spring

---
+++ backend/mp-spring/src/main/java/org/devmandali/magnetPlay/MpSpringBackendApplication.java
@@ -0,0 +1,11 @@
+package org.devmandali.magnetPlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpSpringBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MpSpringBackendApplication.class, args);
+    }
+}
